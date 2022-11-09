50f5a7909595501e97c0139fcfa542d653148609
Vignesh Manivannan <vignesh.manivannan@gmail.com>
2022-11-09 12:45:11 +0530

    mean app

--- src/app/employee/addeditview/addeditview.component.ts
+++ src/app/employee/addeditview/addeditview.component.ts
@@ -18,74 +18,81 @@ export class AddeditviewComponent implements OnInit {
   submitted = false;
 
   constructor(
-      private formBuilder: FormBuilder,
-      private route: ActivatedRoute,
-      private router: Router,
-      private employeeService: employeeService,
-  ) {}
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private employeeService: employeeService,
+  ) { }
 
   ngOnInit() {
     //check id is present in route params
-      this.id = this.route.snapshot.params['id'];
-      this.isAddMode = !this.id;
-      
-      const formOptions: AbstractControlOptions = { validators: null };
-      this.form = this.formBuilder.group({
-          id: ['', Validators.required],
-          fullName: ['', Validators.required],
-          email: ['', [Validators.required, Validators.email]],
-          role: ['', Validators.required],
-      }, formOptions);
+    this.id = this.route.snapshot.params['id'];
+    this.isAddMode = !this.id;
 
-      if (!this.isAddMode) {
-            this.employeeService.listRecords.forEach((element: any) => {
-              if(element['id']==this.id){
-                this.form = this.formBuilder.group({
-                  id: element.id,
-                  fullName: element.fullName,
-                  email: element.email,
-                  role: element.role,
-              }, formOptions);
-              }
-            });
+    const formOptions: AbstractControlOptions = { validators: null };
+    this.form = this.formBuilder.group({
+      id: ['', Validators.required],
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      role: ['', Validators.required],
+    }, formOptions);
+
+    if (!this.isAddMode) {
+      this.employeeService.listRecords.forEach((element: any) => {
+        if (element['id'] == this.id) {
+          this.form = this.formBuilder.group({
+            id: element.id,
+            fullName: element.fullName,
+            email: element.email,
+            role: element.role,
+          }, formOptions);
+        }
+      });
+    }
   }
-}
 
   // get attribute for form controls
   get f() { return this.form.controls; }
 
   onSubmit() {
-      this.submitted = true;
+    this.submitted = true;
 
-      // form validation check
-      // if (this.form.invalid) {
-      //     return;
-      // }
-
-      this.loading = true;
-      if (this.isAddMode) {
-          this.createEmployee();
-      } else {
-          this.updateEmployee();
+    // form validation check
+    if (this.form.invalid) {
+        return;
+    }
+    this.employeeService.listRecords.forEach((element: any) => {
+      if (element['id'] == this.form.value["id"]) {
+        alert("Employee ID " + element['id'] +  " is already present in the database. Please enter unique Employee ID")
+        this.submitted = false;
       }
+    });
+    if (!this.submitted)
+      return;
+    this.loading = true;
+    if (this.isAddMode) {
+      this.createEmployee();
+    } else {
+      this.updateEmployee();
+    }
   }
 
   private createEmployee() {
-      this.employeeService.create(this.form.value)
-          .subscribe(res => {
-            alert("Employee created successfully");
-              this.router.navigate(['../'], { relativeTo: this.route });
-          })
-          .add(() => this.loading = false);
+    this.employeeService.create(this.form.value)
+      .subscribe(res => {
+        alert("Employee created successfully");
+        this.router.navigate(['../'], { relativeTo: this.route });
+      })
+      .add(() => this.loading = false);
   }
 
   private updateEmployee() {
-      this.employeeService.update(this.form.value)
-          .subscribe(() => {
-            alert("Employee record updated successfully");
-              this.router.navigate(['../../'], { relativeTo: this.route });
-          })
-          .add(() => this.loading = false);
+    this.employeeService.update(this.form.value)
+      .subscribe(() => {
+        alert("Employee record updated successfully");
+        this.router.navigate(['../../'], { relativeTo: this.route });
+      })
+      .add(() => this.loading = false);
   }
 }
 
