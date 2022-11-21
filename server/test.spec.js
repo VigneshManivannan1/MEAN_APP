543e3baaa000cd8cde994cefd7e8a85074a46fee
Vignesh Manivannan <vignesh.manivannan@gmail.com>
2022-11-21 18:04:32 +0530

    test case

--- server/test.spec.js
+++ server/test.spec.js
@@ -1,7 +1,44 @@
-describe("Testing with Jest", () => {
-    test("createEmployee", () => {
+import request from 'supertest';
+import express from 'express';
+import router from '../api.js';
 
-      expect(createEmployee).toEqual('');
-    });
+const app = new express();
+app.use('/', router);
+
+describe("Testing with Jest", () => {
+  test('responds to get emaployees', async () => {
+    const res = await request(app).get('/employee');
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('responds to post createEmployee', async () => {
+    const res = await request(app).post('/createEmployee');
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('responds to post authEmployee', async () => {
+    const res = await request(app).post('/createEmployee');
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('responds to post registerEmployee', async () => {
+    const res = await request(app).post('/registerEmployee');
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('responds to post verifyToken', async () => {
+    const res = await request(app).post('/verifyToken');
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('responds to put updateEmployee', async () => {
+    const res = await request(app).put('/updateEmployee');
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('responds to post deleteEmployee', async () => {
+    const res = await request(app).delete('/deleteEmployee');
+    expect(res.statusCode).toBe(200);
+  });
 
   });
